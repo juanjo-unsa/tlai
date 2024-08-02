709981e0b8e37764f3d5e61ebf061d3673f46436
Juan José Aguirre <89435155+juanjo-unsa@users.noreply.github.com>
2024-08-02 10:39:17 -0300

    Update AlinRC.py

--- AlinRC.py
+++ AlinRC.py
@@ -5,19 +5,15 @@ def reorganizar_secuencia(secuencia):
     posicion_mas = secuencia.find("+")
     secuencia_reorganizada = secuencia[posicion_mas:] + secuencia[:posicion_mas]
     return secuencia_reorganizada
-
 def reorganizar_archivo_fasta(archivo_entrada, archivo_salida):
     secuencias_reorganizadas = []
-
     for registro in SeqIO.parse(archivo_entrada, "fasta"):
         id_secuencia = registro.id
         secuencia_original = str(registro.seq)
         secuencia_reorganizada = reorganizar_secuencia(secuencia_original)
         secuencia_reorganizada = SeqIO.SeqRecord(Seq(secuencia_reorganizada), id=id_secuencia)
         secuencias_reorganizadas.append(secuencia_reorganizada)
-
     SeqIO.write(secuencias_reorganizadas, archivo_salida, "fasta")
-
 archivo_entrada = ""
 archivo_salida = archivo_entrada.replace(".fasta", "-a.fasta")
 reorganizar_archivo_fasta(archivo_entrada, archivo_salida)
